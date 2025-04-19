29b1d927c04419d2bd19593b813bca466778f0c8
ALFzz <91682563+ALFzz@users.noreply.github.com>
2025-04-19 23:18:15 +0300

    TheReviews page done (ReviewItem added))

--- cooking_site/src/App.vue
+++ cooking_site/src/App.vue
@@ -3,14 +3,14 @@
 import TheHeader from '@/components/TheHeader.vue'
 import TheFooter from '@/components/TheFooter.vue'
 import TheMain from '@/pages/TheMain.vue'
-import TheReviewsBanner from '@/components/TheReviewsBanner.vue'
+import TheReviews from '@/pages/TheReviews.vue'
 </script>
 
 <!--flex-1 элемент увеличиваться и уменьшаться по мере необходимости, игнорируя его начальный размер-->
 
 <template>
   <TheHeader/>
-    <TheReviewsBanner/>
+    <TheReviews/>
   <TheFooter/>
 
 
---
+++ cooking_site/src/components/ReviewItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import { HeartIcon } from '@heroicons/vue/24/solid/index.js'
+
+defineProps({
+  review: String
+})
+</script>
+
+<template>
+  <div class="block px-40 pt-20 pb-10">
+    <div class="flex flex-col">
+      <div class="flex flex-row gap-20 items-center justify-between">
+        <h3 class="w-11/12 text-[24px]">{{review}}</h3>
+        <HeartIcon class=" w-10 h-10"/>
+      </div>
+      <div class="h-0.5 bg-black mt-8 w-full"></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
