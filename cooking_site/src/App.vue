438e17ff4bcbca5168077f18f2c7df5230a62a51
ALFzz <91682563+ALFzz@users.noreply.github.com>
2025-04-19 15:19:00 +0300

    application initialized

---
+++ cooking_site/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <header class="flex justify-center h-20 bg-white">
+    <h1 class="pr-2 ">рецепты</h1>
+    <h1 class="pr-2">о нас</h1>
+    <h1 class="pr-2">личный кабинет</h1>
+    <h1 class="pr-2">отзывы</h1>
+  </header>
+</template>
+
