e1a08cf72643cb78d9fee90f6fb09e375fa1b84f
ALFzz <91682563+ALFzz@users.noreply.github.com>
2025-04-19 16:33:28 +0300

    header added

--- cooking_site/src/App.vue
+++ cooking_site/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
 
+import TheHeader from '@/components/TheHeader.vue'
 </script>
 
 <template>
-  <header class="flex justify-center h-20 bg-white">
-    <h1 class="pr-2 ">рецепты</h1>
-    <h1 class="pr-2">о нас</h1>
-    <h1 class="pr-2">личный кабинет</h1>
-    <h1 class="pr-2">отзывы</h1>
-  </header>
+  <TheHeader/>
 </template>
 
---
+++ cooking_site/src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { MagnifyingGlassCircleIcon } from '@heroicons/vue/24/solid/index.js'
+</script>
+
+<template>
+  <header>
+    <div class="flex items-center justify-end  w-full h-20 bg-white">
+      <div class="flex">
+        <input type="text" placeholder="Поиск" class="pr-10 border-gray-400 placeholder: pl-[14px] border-2 py-1 rounded-3xl">
+        <MagnifyingGlassCircleIcon class="h-12 w-12 text-cyan-40"/>
+      </div>
+
+      <div class="flex absolute left-1/2 transform -translate-x-1/2">
+        <h1 class="pr-2 ">рецепты</h1>
+        <h1 class="pr-2">о нас</h1>
+        <h1 class="pr-2">личный кабинет</h1>
+        <h1 class="">отзывы</h1>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
