db0b0f531634c8f0ac6c830bcf37da6c1a47fc12
ALFzz <91682563+ALFzz@users.noreply.github.com>
2025-04-19 17:27:28 +0300

    TheDelicious Block done

--- cooking_site/src/App.vue
+++ cooking_site/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 
 import TheHeader from '@/components/TheHeader.vue'
+import TheDelicious from '@/components/TheDelicious.vue'
 </script>
 
 <template>
   <TheHeader/>
+  <TheDelicious/>
 </template>
 
