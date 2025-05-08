8f0592689607f990de4bfb11e7d98af07fb325e7
ALFzz <qiranem@gmail.com>
2025-05-08 16:11:40 +0300

    TheCategories Page adaptability done

--- cooking_site/src/components/TheCategoriesBanner.vue
+++ cooking_site/src/components/TheCategoriesBanner.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="relative transition duration-300 hover:brightness-125 cursor-pointer">
     <img :src="img" :alt="label" class="w-full ">
     <div class="absolute flex flex-row items-center justify-center w-full text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <h1 class="font-semibold text-[100px]">{{label}}</h1>
+      <h1 class="font-semibold text-[100px] max-sm:text-[45px] max-[350px]:text-[30px]">{{label}}</h1>
     </div>
   </div>
 </template>
